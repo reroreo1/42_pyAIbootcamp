dc9906cdc3f9f8d9575119a2e9498dc1a6ee3585
Reroreo1 <86200459+skyjumper24@users.noreply.github.com>
2022-03-02 14:16:14 +0100

    ex03

---
+++ ex03/count.py
@@ -0,0 +1,26 @@
+import string
+def text_analyzer(jomla):
+    upper = 0
+    lower = 0
+    punc = 0
+    space = 0
+    if (isinstance(jomla, str) == False):
+        print("error")
+        exit(0)
+    print("Python 2.0, released 2000, introduced features like List comprehensions and a garbage collection system capable of collecting reference cycles")
+    for char in jomla:
+        if(char.isupper() == 1):
+            upper += 1
+        elif(char.islower() == 1):
+            lower += 1
+        elif(char.isspace() == 1):
+            space += 1
+        elif(char in string.punctuation):
+            punc  += 1
+    print("The text contains" ,len(jomla), "character(s) :")
+    print("- " , lower," lower letter(s)")
+    print("- " , upper," upper letter(s)")
+    print("- " , punc," punctuation mark(s)")
+    print("- " , space," space(s)")
+
+
