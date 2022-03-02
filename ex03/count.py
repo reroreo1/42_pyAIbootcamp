2c0e149424436365a882a7d76bb198e026831537
Reroreo1 <86200459+skyjumper24@users.noreply.github.com>
2022-03-02 14:40:22 +0100

    final program

--- ex03/count.py
+++ ex03/count.py
@@ -1,4 +1,6 @@
-import string
+import string 
+import sys
+
 def text_analyzer(jomla):
     upper = 0
     lower = 0
@@ -7,6 +9,9 @@ def text_analyzer(jomla):
     if (isinstance(jomla, str) == False):
         print("error")
         exit(0)
+    if (bool(jomla) == False):
+        x = input("what is the text to analyze")
+        jomla = x
     print("Python 2.0, released 2000, introduced features like List comprehensions and a garbage collection system capable of collecting reference cycles")
     for char in jomla:
         if(char.isupper() == 1):
@@ -22,5 +27,12 @@ def text_analyzer(jomla):
     print("- " , upper," upper letter(s)")
     print("- " , punc," punctuation mark(s)")
     print("- " , space," space(s)")
+
+n = len(sys.argv)
+if (n > 2):
+    print("error")
+
+text_analyzer(sys.arg[1])
+
 
 
